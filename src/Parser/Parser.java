9ba0c85cbc521dd1edf52d1bd6e967689ed19f43
user <swyat.usatov@gmail.com>
2020-09-21 09:42:41 +0300

    new functional

--- src/Parser/Parser.java
+++ src/Parser/Parser.java
@@ -23,18 +23,40 @@ public class Parser {
     void parseFile(char delimeter,String mode) throws IOException {
         LinkedList<StringBuilder> linkedList = new LinkedList<>();
         int c;
+        int first_comm_char = -1;
+        int end_comm_char = -1;
+        boolean comment = false;
         boolean active = false;
         boolean del = false;
         int first = -1;
         int second = -1;
         int j = 0;
         StringBuilder buffer = new StringBuilder();
+        StringBuilder buffer_comm = new StringBuilder();
         while ((c = reader.read()) != -1) {
             j++;
 
+            if((char)c == '/'||(char)c=='*'){
+                buffer_comm.append((char)c);
+            }
+            if(buffer_comm.length()>2){
+                buffer_comm = new StringBuilder();
+            }
+            if(buffer_comm.toString().equals("/*")){
+                buffer.deleteCharAt(j-2);
+                comment=true;
+                buffer_comm = new StringBuilder();
 
-            if ((char)c!='\n'){
+            }
+
+            if ((char)c!='\n'&& !comment){
                 buffer.append((char) c);
+            }
+            if(buffer_comm.toString().equals("*/")){
+                j=0;
+
+                comment=false;
+                buffer_comm = new StringBuilder();
             }
             if(first==1 && second!=-1 &&buffer.length()!=0&& !del){
                 buffer.deleteCharAt(0);
@@ -50,7 +72,7 @@ public class Parser {
                 }
                 active = !active;
             }
-            if (c == '\n') {
+            if (c == '\n'&&!comment) {
 
                 linkedList.addLast(buffer.deleteCharAt(buffer.length()-1));
                 buffer = new StringBuilder();
@@ -75,7 +97,7 @@ public class Parser {
                 second = -1;
                 del = false;
             }
-            if (c == delimeter && !active) {
+            if (c == delimeter && !active&& !comment) {
                 j=0;
                 first = -1;
                 second = -1;
